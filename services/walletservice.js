fd0765232e317dcea42bf7c5911dc1ba176d4c58
IAMGOODNESS <goodnesspeters77@gmail.com>
2026-04-25 09:11:51 +0100

    added ssl connections444

--- services/walletservice.js
+++ services/walletservice.js
@@ -5,7 +5,7 @@ class WalletService {
      */
     async createUserWallet(user_id, initial_balance = 0.00, client) {
         const result = await client.query(
-            'INSERT INTO wallets (user_id, balance, pending_balance) VALUES ($1, $2, $3) RETURNING *', 
+            'INSERT INTO wallets (user_id, balance, pendingbalance) VALUES ($1, $2, $3) RETURNING *', 
             [user_id, initial_balance, 0.00]
         );
         return result.rows[0]; 
