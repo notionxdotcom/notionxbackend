f0a1714d0172f789cbbe4bb59ae754f28864883e
IAMGOODNESS <goodnesspeters77@gmail.com>
2026-04-26 00:27:49 +0100

    added get ledger

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,12 +3,14 @@ import express from "express";
 import getMyReferrals from '../Controller/walletcontrollers/getrefferedusers.js';
 import getuserbyid from "../Controller/getuserdetails.js";
 import getMyActiveProducts from "../Controller/walletcontrollers/getproducts.js";
+import getLedger from "../Controller/walletcontrollers/getledger.js";
 const router = express.Router();
 
 
 router.get("/me", getuserbyid);
 router.get("/my-products", getMyActiveProducts);
 router.get('/my-referrals', getMyReferrals);
+router.get('/ledger', getLedger);
 
 
 export default router;
