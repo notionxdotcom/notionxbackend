a4eae47b16e12ef0aaaa210bc164319b34cf0d59
IAMGOODNESS <goodnesspeters77@gmail.com>
2026-05-16 09:03:20 +0100

    moving files to GMT-FRONTEND

--- model/validation.js
+++ model/validation.js
@@ -1,6 +1,6 @@
 import Joi from "joi"
 const validation_schema=Joi.object({
- password:Joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$')).messages({"string.pattern.base": "Password must be at least 8 chars, include uppercase, lowercase, number, and special character"}),
+ password:Joi.string(),
  phoneNumber:Joi.string().max(11).required(),
 referralCode:Joi.string().max(8).allow(null, ''),
 
